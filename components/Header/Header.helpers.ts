54614a7ddeea8ce0241fe8ef8bcb80afd44eaaf9
ozanbilgic <ozan.bilgic@nextedy.com>
2023-02-18 18:19:17 +0100

    Create Header component

---
+++ components/Header/Header.helpers.ts
@@ -0,0 +1,19 @@
+export function getHeaderItems() {
+  return [
+    {
+      title: "/",
+    },
+    {
+      title: "Platforms",
+    },
+    {
+      title: "Stores",
+    },
+    {
+      title: "Genres",
+    },
+    {
+      title: "Developers",
+    },
+  ];
+}
---
+++ components/Header/Header.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import { getHeaderItems } from "./Header.helpers";
+import styles from "./styles.module.scss";
+
+const headerItems = getHeaderItems();
+
+export function Header() {
+  return (
+    <div className={styles["Header"]}>
+      {headerItems.map((item, index) => {
+        return (
+          <Link
+            href={item.title.toLowerCase()}
+            key={index}
+            className={styles["Header__Link"]}
+          >
+            {item.title === "/" ? "Home" : item.title}
+          </Link>
+        );
+      })}
+    </div>
+  );
+}
