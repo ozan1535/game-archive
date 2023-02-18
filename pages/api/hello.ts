2e618f33400eb98592e9b4ce89ee7f630cd6e040
ozanbilgic <ozan.bilgic@nextedy.com>
2023-02-18 18:20:05 +0100

    Refactor default files

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,23 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  name: string
-}
+  name: string;
+};
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const request = async () => {
+    const response = await fetch(
+      `https://api.rawg.io/api/games?key=${process.env.API_KEY}&page_size=15`
+    );
+
+    const data = await response.json();
+
+    res.status(200).json(data);
+  };
+
+  request();
 }
