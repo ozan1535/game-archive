464aedd168145e52679749b49d04765fded71673
ozanbilgic <ozan.bilgic@nextedy.com>
2023-02-23 22:11:36 +0100

    Create Card component

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -1,5 +1,42 @@
+import Image from "next/image";
+import { CardInformationKeyAndValue } from "./CardInformationKeyAndValue";
 import styles from "./styles.module.scss";
 
-export function Card() {
-  return <div className={styles["card"]}>hellloooo</div>;
+const sampleArr = ["adsf", "fewf", "wefdsf"];
+
+export function Card({ data }) {
+  console.log(data, "fewfwe");
+
+  return (
+    <>
+      {data.results.map((item, index) => (
+        <div className={styles["Card"]} key={index}>
+          <Image
+            alt="image"
+            src={item.background_image}
+            className={styles["Card__Picture"]}
+            width={328}
+            height={160}
+          />
+
+          <div className={styles["Card__Information"]}>
+            <div className={styles["Card__Information__Platform"]}>
+              platforms
+            </div>
+            <div className={styles["Card__Information__Name"]}>{item.name}</div>
+            <div className={styles["Card__Information__InfoKey"]}>
+              <CardInformationKeyAndValue name={"Release date:"} />
+              <CardInformationKeyAndValue name={"Genres:"} />
+              <CardInformationKeyAndValue name={"Chart:"} />
+            </div>
+            <div className={styles["Card__Information__InfoValue"]}>
+              <CardInformationKeyAndValue name={item.released} />
+              <CardInformationKeyAndValue name={"fwefew"} />
+              <CardInformationKeyAndValue name={"vewvdsd"} />
+            </div>
+          </div>
+        </div>
+      ))}
+    </>
+  );
 }
---
+++ components/Card/CardInformationKeyAndValue.tsx
@@ -0,0 +1,13 @@
+export function CardInformationKeyAndValue({ name }: { name: string }) {
+  return (
+    <>
+      <div>{name}</div>
+      <hr
+        style={{
+          width: "100%",
+          border: "0.9px solid #1098ad",
+        }}
+      />
+    </>
+  );
+}
