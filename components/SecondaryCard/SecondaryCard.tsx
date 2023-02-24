98a1b526053f98cc7f18105910a981e1f442738c
ozanbilgic <ozan.bilgic@nextedy.com>
2023-02-24 23:34:04 +0100

    Create SecondaryCard component

---
+++ components/SecondaryCard/SecondaryCard.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import styles from "./styles.module.scss";
+
+export function SecondaryCard({ data }) {
+  return (
+    <>
+      {data.results.map((item, index) => (
+        <div key={index} className={styles["SecondaryCard"]}>
+          <div className={styles["SecondaryCard__Name"]}>{item.name}</div>
+          <Image
+            src={item.image_background}
+            alt={item.image_background}
+            width={350}
+            height={350}
+            className={styles["SecondaryCard__Picture"]}
+          />
+        </div>
+      ))}
+    </>
+  );
+}
