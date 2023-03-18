e445a53a73e2b2067d1e80d6aa5ac97cbf4f6fde
ozanbilgic <ozan.bilgic@nextedy.com>
2023-03-18 22:05:15 +0100

    Create Pagination component

---
+++ components/Pagination/Pagination.tsx
@@ -0,0 +1,35 @@
+import { useRouter } from "next/router";
+import ReactPaginate from "react-paginate";
+
+export function Pagination({ count }: { count: number }) {
+  const router = useRouter();
+
+  const divideContentNumbers = Math.floor(count / 20);
+  const divideContentNumbersRemainder = count % 20;
+
+  const handlePageClick = (a: { selected: number }) => {
+    const { selected } = a;
+
+    const url = new URL(router.asPath, window.location.origin);
+    url.searchParams.set("page", `${selected + 1}`);
+
+    // https://nextjs.org/docs/routing/shallow-routing
+    router.push(url, undefined, { shallow: true });
+  };
+
+  return (
+    <ReactPaginate
+      previousLabel={"←"}
+      nextLabel={"→"}
+      pageCount={divideContentNumbers + (divideContentNumbersRemainder ? 1 : 0)}
+      onPageChange={handlePageClick}
+      containerClassName={"pagination"}
+      previousLinkClassName={"pagination__link"}
+      nextLinkClassName={"pagination__link"}
+      disabledClassName={"pagination__link--disabled"}
+      activeClassName={"pagination__link--active"}
+      initialPage={router.query.page ? +router.query.page - 1 : undefined}
+      disableInitialCallback={true}
+    />
+  );
+}
