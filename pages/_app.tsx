a00458dafcd433de26c17ee45db5a36c75fa2c7d
ozanbilgic <ozan.bilgic@nextedy.com>
2023-03-19 15:00:21 +0100

    Get fallback data

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import { NextPage } from "next";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
 import { ReactElement, ReactNode } from "react";
+import { SWRConfig } from "swr";
 import "@/styles/globals.css";
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
@@ -12,7 +14,31 @@ type AppPropsWithLayout = AppProps & {
 };
 
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  const router = useRouter();
+
   const getLayout = Component.getLayout ?? ((page) => page);
 
-  return getLayout(<Component {...pageProps} />);
+  return (
+    <SWRConfig
+      value={{
+        fallback: {
+          [`/api/games/${router.query.page || 1}/`]: pageProps.data,
+          [`/api/platforms/${router.query.page || 1}/`]: pageProps.data,
+          [`/api/developers/${router.query.page || 1}/`]: pageProps.data,
+          [`/api/games/${router.query.page || 1}/platforms/${pageProps.param}`]:
+            pageProps.data,
+          [`/api/games/${router.query.page || 1}/stores/${pageProps.param}`]:
+            pageProps.data,
+          [`/api/games/${router.query.page || 1}/genres/${pageProps.param}`]:
+            pageProps.data,
+          [`/api/games/${router.query.page || 1}/developers/${
+            pageProps.param
+          }`]: pageProps.data,
+        },
+        fetcher: (url) => fetch(url).then((r) => r.json()),
+      }}
+    >
+      {getLayout(<Component {...pageProps} />)}
+    </SWRConfig>
+  );
 }
