02adf2edf68cc68fbe030bdcddc5b4cd6fc20e24
ozanbilgic <ozan.bilgic@nextedy.com>
2023-03-19 15:00:10 +0100

    Create custom hook for swr

---
+++ layouts/LayoutCardPages/hooks/useGetCurrentData.ts
@@ -0,0 +1,16 @@
+import { useRouter } from "next/router";
+import useSWR from "swr";
+
+export function useGetCurrentData(
+  slug: string,
+  page: undefined | string = undefined,
+  param: undefined | number = undefined
+) {
+  const router = useRouter();
+
+  const { data } = useSWR(
+    `/api/${slug}/${router.query.page || 1}/${page ? `${page}/${param}` : ""}`
+  );
+
+  return data;
+}
