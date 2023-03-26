732eebe12cde499f19bf3bf10f2efe346091ea3a
ozanbilgic <ozan.bilgic@nextedy.com>
2023-03-26 22:38:42 +0200

    Add Login and title on header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,3 +1,5 @@
+import { signOut } from "next-auth/react";
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { getHeaderItems } from "./Header.helpers";
 import styles from "./styles.module.scss";
@@ -5,19 +7,43 @@ import styles from "./styles.module.scss";
 const headerItems = getHeaderItems();
 
 export function Header() {
+  const { data: session } = useSession();
+
   return (
     <div className={styles["Header"]}>
-      {headerItems.map((item, index) => {
-        return (
+      <div style={{ fontFamily: "monospace", fontSize: "1.25rem" }}>
+        <Link href="/" className={styles["Header__Link"]}>
+          Game Archive
+        </Link>
+      </div>
+      <div>
+        {headerItems.map((item, index) => {
+          return (
+            <Link
+              href={`/${item.title.toLowerCase()}`}
+              key={index}
+              className={styles["Header__Link"]}
+            >
+              {item.title === "/" ? "Home" : item.title}
+            </Link>
+          );
+        })}
+      </div>
+      <div>
+        {session ? (
           <Link
-            href={`/${item.title.toLowerCase()}`}
-            key={index}
+            href="/login"
             className={styles["Header__Link"]}
+            onClick={() => signOut({ callbackUrl: "/login" })}
           >
-            {item.title === "/" ? "Home" : item.title}
+            Sign out
           </Link>
-        );
-      })}
+        ) : (
+          <Link href="/login" className={styles["Header__Link"]}>
+            Login
+          </Link>
+        )}
+      </div>
     </div>
   );
 }
