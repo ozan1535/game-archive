ea51dfe0e9467c62df93faee80eb8dea7fca449b
ozanbilgic <ozan.bilgic@nextedy.com>
2023-03-26 22:39:44 +0200

    Create login page

---
+++ pages/login.tsx
@@ -0,0 +1,48 @@
+import { signIn, getSession } from "next-auth/react";
+import Image from "next/image";
+import { getLayoutDefault } from "@/layouts/LayoutDefault";
+import styles from "@/styles/Login.module.scss";
+
+export default function Login() {
+  return (
+    <div
+      className={styles["Login"]}
+      onClick={() =>
+        signIn("google", {
+          callbackUrl: window.origin + "/",
+        })
+      }
+    >
+      <button className={styles["Login__Button"]}>
+        <Image
+          src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTgiIGhlaWdodD0iMTgiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PGcgZmlsbD0ibm9uZSIgZmlsbC1ydWxlPSJldmVub2RkIj48cGF0aCBkPSJNMTcuNiA5LjJsLS4xLTEuOEg5djMuNGg0LjhDMTMuNiAxMiAxMyAxMyAxMiAxMy42djIuMmgzYTguOCA4LjggMCAwIDAgMi42LTYuNnoiIGZpbGw9IiM0Mjg1RjQiIGZpbGwtcnVsZT0ibm9uemVybyIvPjxwYXRoIGQ9Ik05IDE4YzIuNCAwIDQuNS0uOCA2LTIuMmwtMy0yLjJhNS40IDUuNCAwIDAgMS04LTIuOUgxVjEzYTkgOSAwIDAgMCA4IDV6IiBmaWxsPSIjMzRBODUzIiBmaWxsLXJ1bGU9Im5vbnplcm8iLz48cGF0aCBkPSJNNCAxMC43YTUuNCA1LjQgMCAwIDEgMC0zLjRWNUgxYTkgOSAwIDAgMCAwIDhsMy0yLjN6IiBmaWxsPSIjRkJCQzA1IiBmaWxsLXJ1bGU9Im5vbnplcm8iLz48cGF0aCBkPSJNOSAzLjZjMS4zIDAgMi41LjQgMy40IDEuM0wxNSAyLjNBOSA5IDAgMCAwIDEgNWwzIDIuNGE1LjQgNS40IDAgMCAxIDUtMy43eiIgZmlsbD0iI0VBNDMzNSIgZmlsbC1ydWxlPSJub256ZXJvIi8+PHBhdGggZD0iTTAgMGgxOHYxOEgweiIvPjwvZz48L3N2Zz4="
+          alt="Google"
+          height={25}
+          width={25}
+        />
+        <span className={styles["Login__Button__Span"]}>
+          Continue with Google
+        </span>
+      </button>
+    </div>
+  );
+}
+
+Login.getLayout = getLayoutDefault;
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+
+  if (session) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+}
