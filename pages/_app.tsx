253a1eb5beb30ec5f84a0bb0fe0e7e618a2ad591
ozanbilgic <ozan.bilgic@nextedy.com>
2023-03-30 22:41:13 +0200

    Remove console.log

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,7 +16,6 @@ type AppPropsWithLayout = AppProps & {
 
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
   const { session } = pageProps;
-  console.log(pageProps, "jjajaja");
 
   const router = useRouter();
 
