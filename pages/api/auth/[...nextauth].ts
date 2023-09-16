b8d1672a2042eb373ed3fbc9e11ce2d35af5d476
ozanbilgic <ozan.bilgic@nextedy.com>
2023-09-16 22:07:56 +0200

    Implement firebase

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,56 +1,21 @@
-// http://localhost:1337/api/auth/local
-// http://localhost:1337/api/auth/local/register
-
-import { NextApiRequest, NextApiResponse } from "next";
-import NextAuth, { NextAuthOptions } from "next-auth";
+import { FirestoreAdapter } from "@auth/firebase-adapter";
+import { cert } from "firebase-admin/app";
+import NextAuth, { AuthOptions } from "next-auth";
+import { Adapter } from "next-auth/adapters";
 import GoogleProvider from "next-auth/providers/google";
-
-export const authOptions: NextAuthOptions = {
+export const authOptions: AuthOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
-
-  callbacks: {
-    // This method is not invoked when you persist sessions in a database.
-    async jwt({ token, user, account }) {
-      if (account) {
-        const res = await fetch(
-          `${process.env.SERVER_LINK}/api/auth/google/callback?access_token=${account.access_token}`
-        );
-        const data = await res.json();
-        /* const { jwt, user } = data;
-        token.jwt = jwt;
-        token.id = user.id; */
-        token.user = data;
-      }
-
-      return token;
-    },
-
-    async session({ session, token, user }) {
-      // Send properties to the client, like an access_token from a provider.
-      /*  session.user.jwt = token.jwt as string;
-      session.user.id = token.id as number; */
-      session.user = token.user;
-      return session;
-    },
-  },
-
-  session: {
-    // The default is `"jwt"`, an encrypted JWT (JWE) stored in the session cookie.
-    // If you use an `adapter` however, we default it to `"database"` instead.
-    // You can still force a JWT session by explicitly defining `"jwt"`.
-    strategy: "jwt",
-  },
-
-  // Not providing any secret or NEXTAUTH_SECRET will throw an error in production.
-  secret: process.env.NEXTAUTH_SECRET,
+  adapter: FirestoreAdapter({
+    credential: cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, "\n"),
+    }),
+  }) as Adapter,
 };
-
-const auth = (req: NextApiRequest, res: NextApiResponse) =>
-  NextAuth(req, res, authOptions);
-
-export default auth;
+export default NextAuth(authOptions);
